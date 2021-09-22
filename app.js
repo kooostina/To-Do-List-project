e30dffeac755d084bc182abbbc67f5a1c31a583f
Olha Kostina <o.kostina777@gmail.com>
2021-09-22 21:10:40 +0300

    ability to add tasks and their styling in js

---
+++ app.js
@@ -0,0 +1,35 @@
+// SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// LISTENERS
+todoButton.addEventListener('click', addToDo);
+
+// FUNCTIONS
+function addToDo(event) {
+  // Stop the default form submitting
+  event.preventDefault();
+  // Create a DIV
+  const toDoDiv = document.createElement('div');
+  toDoDiv.classList.add('todo');
+  // Create a Li
+  const newToDo = document.createElement('li');
+  newToDo.innerText = todoInput.value;
+  newToDo.classList.add('todo-item');
+  toDoDiv.appendChild(newToDo);
+  // Check button
+  const completeButton = document.createElement('button');
+  completeButton.classList.add('complete-btn');
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  toDoDiv.appendChild(completeButton);
+  // Trash button
+  const trashButton = document.createElement('button');
+  trashButton.classList.add('trash-btn');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  toDoDiv.appendChild(trashButton);
+  // Append Div to the list
+  todoList.appendChild(toDoDiv);
+  // Clear the input
+  todoInput.value = "";
+}
